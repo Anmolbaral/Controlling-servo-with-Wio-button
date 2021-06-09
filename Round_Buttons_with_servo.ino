27f3c516b0450cd62ff4d31f43f81b49f22ece67
Anmolbaral <58747993+Anmolbaral@users.noreply.github.com>
2021-06-09 19:52:17 -0400

    Add files via upload

---
+++ Round_Buttons_with_servo.ino
@@ -0,0 +1,37 @@
+#include "Servo.h"
+Servo myservo1;
+
+
+int pos = 0;
+void setup() {
+  Serial.begin(115200);
+  myservo1.attach(D1);
+  pinMode(WIO_5S_UP, INPUT);
+  pinMode(WIO_5S_DOWN,INPUT);
+  pinMode(WIO_5S_LEFT,INPUT);
+  pinMode(WIO_5S_RIGHT,INPUT);
+  pinMode(WIO_5S_PRESS, INPUT);
+
+
+}
+
+void loop() {
+  if(digitalRead(WIO_5S_LEFT) == LOW) {
+    pos = pos-1 ;
+    myservo1.write(pos);
+    
+    delay(100);
+    pos = max(0, min(pos,180));
+    Serial.println(pos);
+  }
+   else if(digitalRead(WIO_5S_RIGHT) == LOW) {
+     pos=pos+1 ;
+     myservo1.write(pos);
+     
+     delay(100);
+     pos = max(0, min(pos,180));
+     Serial.println(pos);
+  }
+ 
+}
+//not_satisfied
